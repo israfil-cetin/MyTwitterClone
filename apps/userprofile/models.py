db3017378893509c39ab3d473a7a078a45af142e
israfil çetin <icetin93@gmail.com>
2021-04-20 00:27:11 +0300

    userprofile app added

---
+++ apps/userprofile/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    follows = models.ManyToManyField('self',related_name='followed_by',symmetrical=False)
+
+
+User.userprofile = property(lambda u:UserProfile.objects.get_or_create(user=u)[0])
