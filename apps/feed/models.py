6a38d5dccd76f80e3b519236a57c7b10798fd461
israfil çetin <icetin93@gmail.com>
2021-04-20 01:05:34 +0300

    feed model created

--- apps/feed/models.py
+++ apps/feed/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Cik(models.Model):
+    body = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, related_name='ciks', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at',)
