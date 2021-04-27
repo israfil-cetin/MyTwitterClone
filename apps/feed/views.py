d69fb3fb03a15088fc95fb65a84e58a97c05f183
israfil çetin <icetin93@gmail.com>
2021-04-27 15:42:29 +0300

    ciks can see on feed

--- apps/feed/views.py
+++ apps/feed/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+
+
+from .models import Cik
+
 # Create your views here.
 
+@login_required
 def feed(request):
-    return render(request, 'feed/feed.html')
+    userids = [request.user.id]
+
+    for users in request.user.userprofile.follows.all():
+        userids.append(users.user.id)
+
+    ciks = Cik.objects.filter(created_by_id__in=userids)
+    return render(request, 'feed/feed.html', {'ciks': ciks})
