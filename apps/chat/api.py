ef760fae15a9fb23cb1f4104054e6f990fbd3f30
israfil çetin <icetin93@gmail.com>
2021-05-31 03:30:28 +0300

    updated chat api (notification)

--- apps/chat/api.py
+++ apps/chat/api.py
@@ -3,6 +3,8 @@ import json
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 
+from apps.notification.utilities import create_notification
+
 from .models import ChatMessage
 
 @login_required
@@ -13,5 +15,11 @@ def api_add_message(request):
 
     message = ChatMessage.objects.create(chat_id=conversation_id, content=content, created_by=request.user)
 
+
+    to_user = None
+
+    for user in message.chat.users.all():
+        if user != request.user:
+            create_notification(request,user, 'message')
 
     return JsonResponse({'success': True})
